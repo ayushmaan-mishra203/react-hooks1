634a67482f76a5b4a78676489244b29743bdd05e
ayushmaan-mishra203 <142373193+ayushmaan-mishra203@users.noreply.github.com>
2024-06-08 21:34:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+// src/app/page.tsx
 
-export default function Home() {
+import Link from 'next/link';
+
+const Home: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>hello</h1>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p className="mt-4 text-xl">
+        Welcome to the home page.
+      </p>
+      <Link href="/about">
+        <span className="mt-4 text-blue-500 hover:underline">Go to About Page</span>
+      </Link>
+    </div>
   );
-}
+};
+
+export default Home;
